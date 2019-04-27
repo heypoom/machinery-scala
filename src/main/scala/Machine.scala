786a45e0b6fd6169610ba12579095445ebf9591f
Phoomparin Mano <phoomparin@gmail.com>
2019-04-27 15:56:57 +0700

    :art: Remove braces

--- src/main/scala/Machine.scala
+++ src/main/scala/Machine.scala
@@ -4,15 +4,12 @@ class Machine {
   var register: mutable.HashMap[Int, Int] = new mutable.HashMap()
   implicit val machine: Machine = this
 
-  def execute(instruction: Instruction, register: Register): Unit = {
+  def execute(instruction: Instruction, register: Register): Unit =
     instruction.compute(register)
-  }
 
-  def execute(instruction: Instruction, dest: Register, value: Int): Unit = {
+  def execute(instruction: Instruction, dest: Register, value: Int): Unit =
     instruction.compute(dest, value)
-  }
 
-  def execute(instruction: Instruction, dest: Register, src: Register): Unit = {
+  def execute(instruction: Instruction, dest: Register, src: Register): Unit =
     instruction.compute(dest, src)
-  }
 }
